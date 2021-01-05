b45e5ed874d4d723c67fde8a110787f83a4816a4
andermsilva <andermarsil@gmail.com>
2021-01-05 11:56:49 -0300

    GET/Orders

--- bakend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ bakend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -35,6 +35,19 @@ public class Order implements Serializable {
 	public Order() {
 		
 	}
+   
+	 
+
+	public Order(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+		super();
+		this.id = id;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.moment = moment;
+		this.status = status;
+	}
+
 
 
 	public Long getId() {
