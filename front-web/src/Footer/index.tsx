ec840a3dc4a0f816dba2ba5df8e0dee0e742c120
andermsilva <andermarsil@gmail.com>
2021-01-06 19:07:18 -0300

    Add routes and StepsHeaders

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -13,15 +13,15 @@ function Footer(){
 
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className='footer-icons'>
-                <a href='http://www.youtube.com/c/DevSuperior' target ='_new'>
+                <a href='https://www.youtube.com/c/DevSuperior' target ='_new'>
                         <YouTubeIcon/>
                 </a>
 
-                <a href='http://www.youtube.com/c/DevSuperior' target ='_new'>
+                <a href='https://www.linkedin.com/school/devsuperior/' target ='_new'>
                         <LinkedinIcon/>
                 </a>
 
-                <a href='http://www.youtube.com/c/DevSuperior' target ="_new">
+                <a href='https://www.instagram.com/devsuperior.ig/' target ="_new">
                         <InstagranIcon/>
                 </a>
 
